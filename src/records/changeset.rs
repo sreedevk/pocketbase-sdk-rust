79d1b1a0e2a3f0fffab3850bb85700a8d4b2ce93
sreedev <sreedevpadmakumar@gmail.com>
2022-10-08 11:07:02 -0400

    removed record operations

--- src/records/changeset.rs
+++
@@ -1,9 +0,0 @@
-use serde::Serialize;
-use crate::users::User;
-
-#[derive(Serialize)]
-pub struct Changeset<'a, T> {
-    pub user:  &'a User<'a>,
-    pub resource: &'a str,
-    pub record: &'a T
-}
--- src/records/mod.rs
+++
@@ -1,5 +0,0 @@
-mod changeset;
-mod collection;
-pub mod operations;
-pub use changeset::Changeset;
-pub use collection::Collection;
