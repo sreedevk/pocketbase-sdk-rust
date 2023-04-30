c4308342d5a27cf117e3e78190ee1b0476fd882c
sreedev <sreedevpadmakumar@gmail.com>
2023-04-30 10:48:16 -0400

    updated examples

--- examples/logs.rs
+++ examples/logs.rs
@@ -1,16 +1,25 @@
-use pocketbase_sdk::admin::Admin;
 use anyhow::Result;
+use pocketbase_sdk::admin::Admin;
 
 fn main() -> Result<()> {
     env_logger::init();
-    let admin = Admin::new("http://localhost:8090", "sreedev@icloud.com", "Sreedev123")?;
-    let logs  = admin.logs().list().page(1).per_page(10).call()?;
-    let somelogid = &logs.items[0].id.clone();
-    let logitem = admin.logs().view(somelogid).call()?;
-    let logstats = admin.logs().statistics().call()?;
 
-    dbg!(logs);
-    dbg!(logstats);
+    // admin authentication
+    let admin = Admin::new("http://localhost:8090")
+        .auth_with_password("sreedev@icloud.com", "Sreedev123")?;
+
+    // list logs
+    let logs = admin.logs().list().page(1).per_page(10).call()?;
+    dbg!(&logs);
+
+    // view log
+    let somelogid = &logs.items[0].id;
+    let logitem = admin.logs().view(somelogid).call()?;
     dbg!(logitem);
+
+    // view log statistics data points
+    let logstats = admin.logs().statistics().call()?;
+    dbg!(logstats);
+
     Ok(())
 }
